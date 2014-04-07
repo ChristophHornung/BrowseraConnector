3a4a527b1f9f048b22e9880b931376da999919b3
Weakegg <christoph-hornung@christoph-hornung.de>
2014-04-07 18:16:45 +0200

    Added documentation

--- BrowseraConnector/ConnectionUrl.cs
+++ BrowseraConnector/ConnectionUrl.cs
@@ -1,16 +1,48 @@
+#region header
+
+//     This file is part of BrowseraConnector.
+// 
+//     BrowseraConnector is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Lesser General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     BrowseraConnector is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Lesser General Public License for more details.
+// 
+//     You should have received a copy of the GNU Lesser General Public License
+//     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
 namespace BrowseraConnector
 {
     /// <summary>
     /// The different urls for the browsera API.
     /// </summary>
-    public static class ConnectionUrl
+    internal static class ConnectionUrl
     {
-        public const string TestRunsUrl = "https://api.browsera.com/v0.1a/web_sites/{1}/test_runs.xml?api_key={0}";
+        /// <summary>
+        /// The URL to access all test runs.
+        /// </summary>
+        internal const string TestRunsUrl = "https://api.browsera.com/v0.1a/web_sites/{1}/test_runs.xml?api_key={0}";
 
-        public const string GetTestRunUrl =
+        /// <summary>
+        /// The URL to access a specific test run.
+        /// </summary>
+        internal const string GetTestRunUrl =
             "https://api.browsera.com/v0.1a/web_sites/{1}/test_runs/{2}.xml?api_key={0}";
 
-        public const string WebSitesUrl = "https://api.browsera.com/v0.1a/web_sites.xml?api_key={0}";
-        public const string GetWebSiteUrl = "https://api.browsera.com/v0.1a/web_sites/{1}.xml?api_key={0}";
+        /// <summary>
+        /// The URL to access all websites.
+        /// </summary>
+        internal const string WebSitesUrl = "https://api.browsera.com/v0.1a/web_sites.xml?api_key={0}";
+
+        /// <summary>
+        /// The URL to access a specific website.
+        /// </summary>
+        internal const string GetWebSiteUrl = "https://api.browsera.com/v0.1a/web_sites/{1}.xml?api_key={0}";
     }
 }
