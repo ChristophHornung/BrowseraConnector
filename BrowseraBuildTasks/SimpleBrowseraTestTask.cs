c482f34aaa224171a7e7dbf48af8946b7d1f591f
Weakegg <christoph-hornung@christoph-hornung.de>
2014-04-09 16:28:38 +0200

    Split the msbuild task into two parts

--- BrowseraBuildTasks/SimpleBrowseraTestTask.cs
+++ BrowseraBuildTasks/SimpleBrowseraTestTask.cs
@@ -29,8 +29,65 @@ namespace BrowseraBuildTasks
     /// <summary>
     /// A build task to automate creation and starts of browsera tasks
     /// </summary>
-    public class AutomatedBrowseraTask : Task
+    public class SimpleBrowseraTestTask : BrowseraBaseTask
     {
+        public SimpleBrowseraTestTask() : base(false)
+        {
+        }
+
+        protected override WebsiteTestConfiguration GetWebsiteTestConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class BrowseraBaseTask : Task
+    {
+        private readonly bool createNewSite;
+
+        protected BrowseraBaseTask(bool createNewSite)
+        {
+            this.createNewSite = createNewSite;
+        }
+
+        public string SiteName { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public string ResultDirectory { get; set; }
+
+        public override bool Execute()
+        {
+            if (!Directory.Exists(this.ResultDirectory))
+            {
+                this.Log.LogMessage(MessageImportance.Low, "Creating result directory " + this.ResultDirectory);
+                Directory.CreateDirectory(this.ResultDirectory);
+            }
+
+            BrowseraTestExecutor executor = new BrowseraTestExecutor(this.ApiKey);
+            if (this.createNewSite)
+            {
+                executor.CreateSiteAndRunTestAndPollResult(this.GetWebsiteTestConfiguration(), this.ResultDirectory);
+            }
+            else
+            {
+                executor.RunTestOnSiteAndPollResult(this.SiteName, this.ResultDirectory);
+            }
+            return true;
+        }
+
+        protected abstract WebsiteTestConfiguration GetWebsiteTestConfiguration();
+    }
+
+    /// <summary>
+    /// A build task to automate creation and starts of browsera tasks
+    /// </summary>
+    public class ComplexBrowseraTestTask : BrowseraBaseTask
+    {
+        public ComplexBrowseraTestTask() : base(true)
+        {
+        }
+
         public string CustomJavascript { get; set; }
 
         public string LoginUrl { get; set; }
@@ -38,12 +95,6 @@ namespace BrowseraBuildTasks
         public string[] Pages { get; set; }
 
         public string[] Browsers { get; set; }
-
-        public string ApiKey { get; set; }
-
-        public string ResultDirectory { get; set; }
-
-        public string SiteName { get; set; }
 
         public string BaseLineBrowser { get; set; }
 
@@ -57,20 +108,7 @@ namespace BrowseraBuildTasks
 
         public static string Password { get; set; }
 
-        public override bool Execute()
-        {
-            if (!Directory.Exists(this.ResultDirectory))
-            {
-                this.Log.LogMessage(MessageImportance.Low, "Creating result directory " + this.ResultDirectory);
-                Directory.CreateDirectory(this.ResultDirectory);
-            }
-
-            BrowseraTestExecutor executor = new BrowseraTestExecutor(this.ApiKey);
-            executor.CreateSiteAndRunTestAndPollResult(this.GetWebsiteTestConfiguration(), this.ResultDirectory);
-            return true;
-        }
-
-        private WebsiteTestConfiguration GetWebsiteTestConfiguration()
+        protected override WebsiteTestConfiguration GetWebsiteTestConfiguration()
         {
             return new WebsiteTestConfiguration()
             {
@@ -80,7 +118,7 @@ namespace BrowseraBuildTasks
                     BrowserValues = new List<string>(this.Browsers)
                 },
                 MaxCrawlPages = 0,
-                Name = this.SiteName + "(" + DateTime.UtcNow + ")",
+                Name = this.SiteName + " (" + DateTime.UtcNow + ")",
                 Urls = new Urls() {UrlValues = new List<string>(this.Pages)},
                 SiteLoginConfiguration = this.GetLoginConfiguration()
             };
